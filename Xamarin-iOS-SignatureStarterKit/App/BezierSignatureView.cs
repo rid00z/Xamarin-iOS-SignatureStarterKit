b06d505ef1688120a18e634a9df921dd60b99a1a
Michael Ridland <rid00z@gmail.com>
2014-04-11 15:01:26 +1000

    Converted tabs to spaces

--- Xamarin-iOS-SignatureStarterKit/App/BezierSignatureView.cs
+++ Xamarin-iOS-SignatureStarterKit/App/BezierSignatureView.cs
@@ -6,98 +6,98 @@ using MonoTouch.Foundation;
 
 namespace XamariniOSSignatureStarterKit
 {
-	[Register]
-	public class BezierSignatureView : UIView
-	{		
-		public bool IsSigning { get; set; }
-		private PointF _touchLocation;
-		private PointF _prevTouchLocation;
-		private CGPath _drawPath;
-		private UIPanGestureRecognizer _panner = null;
+    [Register]
+    public class BezierSignatureView : UIView
+    {       
+        public bool IsSigning { get; set; }
+        private PointF _touchLocation;
+        private PointF _prevTouchLocation;
+        private CGPath _drawPath;
+        private UIPanGestureRecognizer _panner = null;
 
-		[Export ("BezierSignatureViewPan")]
-		protected void pan(UIPanGestureRecognizer sender)
-		{
-			if (IsSigning) {
-				_touchLocation = sender.LocationInView (this);
-				PointF mid = BezierSignatureView.MidPoint (_touchLocation, _prevTouchLocation);
+        [Export ("BezierSignatureViewPan")]
+        protected void pan(UIPanGestureRecognizer sender)
+        {
+            if (IsSigning) {
+                _touchLocation = sender.LocationInView (this);
+                PointF mid = BezierSignatureView.MidPoint (_touchLocation, _prevTouchLocation);
 
-				switch (sender.State) {
-				case UIGestureRecognizerState.Began:
-					{ 
-						_drawPath.MoveToPoint (_touchLocation);
-						break; 
-					}
-				case UIGestureRecognizerState.Changed:
-					{
-						_drawPath.AddQuadCurveToPoint (_prevTouchLocation.X, _prevTouchLocation.Y, mid.X, mid.Y);					
-						IsSigning = true;
-						break; 
-					}
-				default:
-					{
-						break; }
+                switch (sender.State) {
+                case UIGestureRecognizerState.Began:
+                    { 
+                        _drawPath.MoveToPoint (_touchLocation);
+                        break; 
+                    }
+                case UIGestureRecognizerState.Changed:
+                    {
+                        _drawPath.AddQuadCurveToPoint (_prevTouchLocation.X, _prevTouchLocation.Y, mid.X, mid.Y);                   
+                        IsSigning = true;
+                        break; 
+                    }
+                default:
+                    {
+                        break; }
 
-				}
-				_prevTouchLocation = _touchLocation;
-				SetNeedsDisplay ();
-			}
-		}
+                }
+                _prevTouchLocation = _touchLocation;
+                SetNeedsDisplay ();
+            }
+        }
 
-		public void Clear ()
-		{
-			_drawPath.Dispose ();
-			_drawPath = new CGPath ();
-			SetNeedsDisplay ();			
-		}
+        public void Clear ()
+        {
+            _drawPath.Dispose ();
+            _drawPath = new CGPath ();
+            SetNeedsDisplay ();         
+        }
 
-		public BezierSignatureView (RectangleF frame, bool isSigning = true) : base(frame)
-		{
-			IsSigning = isSigning;
-			_drawPath = new CGPath ();
-			BackgroundColor = UIColor.White;
-			MultipleTouchEnabled = false;
+        public BezierSignatureView (RectangleF frame, bool isSigning = true) : base(frame)
+        {
+            IsSigning = isSigning;
+            _drawPath = new CGPath ();
+            BackgroundColor = UIColor.White;
+            MultipleTouchEnabled = false;
 
-			_panner = new UIPanGestureRecognizer (this, new MonoTouch.ObjCRuntime.Selector("BezierSignatureViewPan"));
-			_panner.MaximumNumberOfTouches = _panner.MinimumNumberOfTouches = 1;
-			AddGestureRecognizer (_panner);
-		}
+            _panner = new UIPanGestureRecognizer (this, new MonoTouch.ObjCRuntime.Selector("BezierSignatureViewPan"));
+            _panner.MaximumNumberOfTouches = _panner.MinimumNumberOfTouches = 1;
+            AddGestureRecognizer (_panner);
+        }
 
-		public UIImage GetDrawingImage()
-		{
-			UIImage returnImg = null;
+        public UIImage GetDrawingImage()
+        {
+            UIImage returnImg = null;
 
-			UIGraphics.BeginImageContext (this.Bounds.Size);
-			using (CGContext context = UIGraphics.GetCurrentContext()) 
-			{
-				context.SetStrokeColor (UIColor.Black.CGColor);
-				context.SetLineWidth (5f);
-				context.AddPath (this._drawPath);
-				context.StrokePath ();
-				returnImg = UIGraphics.GetImageFromCurrentImageContext ();
-			}
-			UIGraphics.EndImageContext ();
-			return returnImg;
-		}
+            UIGraphics.BeginImageContext (this.Bounds.Size);
+            using (CGContext context = UIGraphics.GetCurrentContext()) 
+            {
+                context.SetStrokeColor (UIColor.Black.CGColor);
+                context.SetLineWidth (5f);
+                context.AddPath (this._drawPath);
+                context.StrokePath ();
+                returnImg = UIGraphics.GetImageFromCurrentImageContext ();
+            }
+            UIGraphics.EndImageContext ();
+            return returnImg;
+        }
 
-		public override void Draw (RectangleF rect)
-		{
-			using (CGContext context = UIGraphics.GetCurrentContext()) 
-			{
-				context.SetStrokeColor (UIColor.Black.CGColor);
-				context.SetLineWidth (5f);
-				context.AddPath (this._drawPath);
-				context.StrokePath ();
-			}
-		}
+        public override void Draw (RectangleF rect)
+        {
+            using (CGContext context = UIGraphics.GetCurrentContext()) 
+            {
+                context.SetStrokeColor (UIColor.Black.CGColor);
+                context.SetLineWidth (5f);
+                context.AddPath (this._drawPath);
+                context.StrokePath ();
+            }
+        }
 
-		public static PointF MidPoint(PointF p0, PointF p1) {
-			return new PointF() {
-				X = (float)((p0.X + p1.X) / 2.0),
-				Y = (float)((p0.Y + p1.Y) / 2.0)
-			};
-		}
+        public static PointF MidPoint(PointF p0, PointF p1) {
+            return new PointF() {
+                X = (float)((p0.X + p1.X) / 2.0),
+                Y = (float)((p0.Y + p1.Y) / 2.0)
+            };
+        }
 
-	}
+    }
 }
 
--- Xamarin-iOS-SignatureStarterKit/App/SignViewController.cs
+++ Xamarin-iOS-SignatureStarterKit/App/SignViewController.cs
@@ -6,101 +6,101 @@ using iTextSharp.text.pdf;
 
 namespace XamariniOSSignatureStarterKit
 {
-	public class SignViewController : UIViewController 
-	{
-		public SignViewController ()
-		{
-		}
+    public class SignViewController : UIViewController 
+    {
+        public SignViewController ()
+        {
+        }
 
-		public override void ViewDidLoad ()
-		{
-			base.ViewDidLoad ();
+        public override void ViewDidLoad ()
+        {
+            base.ViewDidLoad ();
 
-			var signButton = new UIBarButtonItem ("Sign PDF", UIBarButtonItemStyle.Plain, (o,e) => ShowPDFForSigning ());
-			NavigationItem.RightBarButtonItem = signButton;
-		}
+            var signButton = new UIBarButtonItem ("Sign PDF", UIBarButtonItemStyle.Plain, (o,e) => ShowPDFForSigning ());
+            NavigationItem.RightBarButtonItem = signButton;
+        }
 
-		public async void ShowPDFForSigning()
-		{
-			var currentFilePath = "Salesinvoice.pdf";
+        public async void ShowPDFForSigning()
+        {
+            var currentFilePath = "Salesinvoice.pdf";
 
-			QLPreviewItemBundle prevItem = new QLPreviewItemBundle ("Salesinvoice.pdf", currentFilePath);
-			QLPreviewController previewController = new QLPreviewController ();
-			previewController.DataSource = new PreviewControllerDS (prevItem);
+            QLPreviewItemBundle prevItem = new QLPreviewItemBundle ("Salesinvoice.pdf", currentFilePath);
+            QLPreviewController previewController = new QLPreviewController ();
+            previewController.DataSource = new PreviewControllerDS (prevItem);
 
-			NavigationController.PushViewController (previewController, true);
+            NavigationController.PushViewController (previewController, true);
 
-			//this adds a button to the QLPreviewController, but it has to wait until after it's been loaded 
-			//I'm not sure if there's a better way to do this.
-			await System.Threading.Tasks.Task.Run( () =>
-				{
-					System.Threading.Thread.Sleep( 500 );
-					for (int i = 0; i < 10; i++)
-					{
-						System.Threading.Thread.Sleep( 500 );
-						InvokeOnMainThread( () =>
-							{
-								if (previewController.NavigationItem.RightBarButtonItems.Length == 1)
-								{
-									var signButton = new UIBarButtonItem( UIBarButtonSystemItem.Compose, (o, e ) =>
-										{
-											SignPDF();
-										} );
+            //this adds a button to the QLPreviewController, but it has to wait until after it's been loaded 
+            //I'm not sure if there's a better way to do this.
+            await System.Threading.Tasks.Task.Run( () =>
+                {
+                    System.Threading.Thread.Sleep( 500 );
+                    for (int i = 0; i < 10; i++)
+                    {
+                        System.Threading.Thread.Sleep( 500 );
+                        InvokeOnMainThread( () =>
+                            {
+                                if (previewController.NavigationItem.RightBarButtonItems.Length == 1)
+                                {
+                                    var signButton = new UIBarButtonItem( UIBarButtonSystemItem.Compose, (o, e ) =>
+                                        {
+                                            SignPDF();
+                                        } );
 
-									previewController.NavigationItem.RightBarButtonItems = 
-										new UIBarButtonItem[] { signButton, previewController.NavigationItem.RightBarButtonItems[0] };								
-								}
-							} );
-					}
-				} );
-		}
+                                    previewController.NavigationItem.RightBarButtonItems = 
+                                        new UIBarButtonItem[] { signButton, previewController.NavigationItem.RightBarButtonItems[0] };                              
+                                }
+                            } );
+                    }
+                } );
+        }
 
-		void SignPDF()
-		{
-			var signViewController = new SignatureViewController("Michael Ridland");
-			signViewController.SignatureCompleted += SignatureWasCompleted;
+        void SignPDF()
+        {
+            var signViewController = new SignatureViewController("Michael Ridland");
+            signViewController.SignatureCompleted += SignatureWasCompleted;
 
-			NavigationController.PopViewControllerAnimated( false );
-			NavigationController.PushViewController( signViewController, true );
-		}
+            NavigationController.PopViewControllerAnimated( false );
+            NavigationController.PushViewController( signViewController, true );
+        }
 
-		void SignatureWasCompleted(byte[] signatureImage, string name)
-		{
-			NavigationController.PopViewControllerAnimated( false );
+        void SignatureWasCompleted(byte[] signatureImage, string name)
+        {
+            NavigationController.PopViewControllerAnimated( false );
 
-			var dirPath = Path.Combine (System.Environment.GetFolderPath (Environment.SpecialFolder.Personal), "SignedPDF");
+            var dirPath = Path.Combine (System.Environment.GetFolderPath (Environment.SpecialFolder.Personal), "SignedPDF");
 
-			if (!Directory.Exists (dirPath))
-				Directory.CreateDirectory (dirPath);
+            if (!Directory.Exists (dirPath))
+                Directory.CreateDirectory (dirPath);
 
-			string fileName = string.Format("result-{0}.pdf", Guid.NewGuid().ToString());
-			var signedFilePath = Path.Combine (dirPath, fileName);
+            string fileName = string.Format("result-{0}.pdf", Guid.NewGuid().ToString());
+            var signedFilePath = Path.Combine (dirPath, fileName);
 
-			if (File.Exists(signedFilePath))
-				File.Delete(signedFilePath);
+            if (File.Exists(signedFilePath))
+                File.Delete(signedFilePath);
 
-			using (Stream inputPdfStream = new FileStream("Salesinvoice.pdf", FileMode.Open, FileAccess.Read, FileShare.Read))				
-			using (Stream outputPdfStream = new FileStream(signedFilePath, FileMode.Create, FileAccess.Write, FileShare.None))
-			{
-				var reader = new PdfReader(inputPdfStream);
-				var stamper = new PdfStamper(reader, outputPdfStream);
-				var pdfContentByte = stamper.GetOverContent(1);
+            using (Stream inputPdfStream = new FileStream("Salesinvoice.pdf", FileMode.Open, FileAccess.Read, FileShare.Read))              
+            using (Stream outputPdfStream = new FileStream(signedFilePath, FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                var reader = new PdfReader(inputPdfStream);
+                var stamper = new PdfStamper(reader, outputPdfStream);
+                var pdfContentByte = stamper.GetOverContent(1);
 
-				iTextSharp.text.Image image = iTextSharp.text.Image.GetInstance(signatureImage);
-				image.SetAbsolutePosition(90, 90);
-				var width = 200F;
-				image.ScaleToFit(width, (float)(width * (200.0 / 480.0)));
-				pdfContentByte.AddImage(image);
-				stamper.Close();
-			}
+                iTextSharp.text.Image image = iTextSharp.text.Image.GetInstance(signatureImage);
+                image.SetAbsolutePosition(90, 90);
+                var width = 200F;
+                image.ScaleToFit(width, (float)(width * (200.0 / 480.0)));
+                pdfContentByte.AddImage(image);
+                stamper.Close();
+            }
 
-			//Display signature after saved
-			QLPreviewItemFileSystem prevItem = new QLPreviewItemFileSystem (fileName, signedFilePath);
-			QLPreviewController previewController = new QLPreviewController ();
-			previewController.DataSource = new PreviewControllerDS (prevItem);
-			NavigationController.SetNavigationBarHidden( false, true );
-			NavigationController.PushViewController (previewController, true);
-		}
-	}
+            //Display signature after saved
+            QLPreviewItemFileSystem prevItem = new QLPreviewItemFileSystem (fileName, signedFilePath);
+            QLPreviewController previewController = new QLPreviewController ();
+            previewController.DataSource = new PreviewControllerDS (prevItem);
+            NavigationController.SetNavigationBarHidden( false, true );
+            NavigationController.PushViewController (previewController, true);
+        }
+    }
 }
 
